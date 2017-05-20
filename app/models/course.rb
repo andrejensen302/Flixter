aace4e1f5e7489903b5cdfee0be86327a2869b07
Andre Jensen <andrejensen302@gmail.com>
2017-05-20 21:44:57 +0000

    added validations to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true #course has title when instructor creates a course
+  validates :description, presence: true #course has description when instructor creates course
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+
 end
