32f2bd60851292291851a88c354bc6d3aedd298f
Andre Jensen <andrejensen302@gmail.com>
2017-05-23 03:41:52 +0000

    added sections to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :sections
 
   validates :title, presence: true #course has title when instructor creates a course
   validates :description, presence: true #course has description when instructor creates course
